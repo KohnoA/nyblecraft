3a5467d5964595f6db2d2a3532387a6cc5139233
KohnoA <san.kohno@yandex.ru>
2023-12-05 01:24:32 +0300

    feat: created typed redux hooks

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,2 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-
-const rootReducer = combineReducers({});
-
-export const store = configureStore({
-  reducer: rootReducer,
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export { store } from './store';
+export { useAppDispatch, useAppSelector } from './hooks';
